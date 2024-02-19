c89674e0cdef01da6ebaffd1c4d9248963d34a10
Kingsley Kim <kingsleykimm@gmail.com>
2024-02-19 00:40:25 -0500

    Added action metric

--- observer.py
+++ observer.py
@@ -11,9 +11,11 @@ class ContObserver(EnvLoopObserver):
     def __init__(self):
         self.observations = None
     def observe_first(self, env : dm_env.Environment, timestep: dm_env.TimeStep, action: np.ndarray) -> None:
-        self.observations = List[Dict[str, float]]
+        self.actions = List[np.ndarray]
+        self.timesteps = List[dm_env.TimeStep]
         self.radii = List[float]
         self.angles = List[float]
+        # self.environments = List[dm_env.Environment]
         # timestep contains all the information
         # current_timestep = Dict[str, float]
         observation = timestep.observation
@@ -27,6 +29,8 @@ class ContObserver(EnvLoopObserver):
         radius = math.sqrt(math.pow(x_pos, 2) + math.pow(y_pos, 2))
         self.radii.append(radius)
         self.angles.append(angle)
+        self.timesteps.append(timestep)
+        self.actions.append(action)
         # self.observations = [current_timestep]
     def observe(self, env: dm_env.Environment, timestep: dm_env.TimeStep,
               action: np.ndarray) -> None:
@@ -41,13 +45,18 @@ class ContObserver(EnvLoopObserver):
         angle = math.atan2(y_pos, x_pos)
         self.radii.append(radius)
         self.angles.append(angle)
+        self.timesteps.append(timestep)
+        self.actions.append(action)
+        # self.environments.append(env)
         # current_timestep["radius"] = radius
         # current_timestep["angle"] = angle
         # self.observations.append(current_timestep)
     def get_metrics(self) -> Dict[str, float]:
         return {
                 'radius' : self.radii,
-                'angle': self.angles
+                'angle': self.angles,
+                'timestep': self.timesteps,
+                'action': self.actions,
         }
     def get_episode_obs(self) -> List[Dict[str, float]]:
         return self.observations
